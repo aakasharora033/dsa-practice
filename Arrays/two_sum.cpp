529a523ae8da448e6f167139b57dde38ddfe4905
Aakash Arora <137984101+aakasharora033@users.noreply.github.com>
2026-04-03 11:11:51 +0530

    Add twoSum function for solving Two Sum problem
    Implemented twoSum function using a hashmap to find indices of two numbers that add up to the target.

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,1 +1,24 @@
+// Problem: Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+// Approach: Hashmap to store complement
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+
+// Intuition: For each element, check if its complement already exists in hashmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int,int> mp;
+      
+        for(int i=0; i<nums.size(); i++){
+            int complement = target-nums[i];
+          
+            if(mp.find(complement)!=mp.end()){
+                return {mp[complement],i};
+            }
+            mp[nums[i]] = i;
+        }
+        return{};   
+    }
+};
